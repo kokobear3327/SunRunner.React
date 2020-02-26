6d8b93a25401c267051d3ead4db967b1adc395f8
RichardPitts1 <Richardepitts@gmail.com>
2020-02-25 23:28:25 -0600

    -

--- src/consumer/consumer-with-dock-opened/consumer-with-menu-overflow.js
+++ src/consumer/consumer-with-dock-opened/consumer-with-menu-overflow.js
@@ -1,9 +1,6 @@
 import './consumer-with-dock-opened.css';
 import React from 'react'
-import consumer from '../../images/Consumer.png'; 
-import TreeButton from '../../shared/tree-button/tree';
-import MedicalInfoButton from '../../shared/medical-info-button/medical-info-button';
-// import ConsumerWithDockOpened from '../../shared/consumer-dock/consumer-with-dock-opened/consumer-with-dock-opened';
+import consumer from '../../images/ConsumerBlank.png'; 
 import MenuOverflowOpen from '../../shared/mac-bar/menu-overflow-open/menu-overflow-open';
 
 // So you would add an onclick event where the router would take you to the route specified. 
@@ -12,8 +9,6 @@ function ConsumerWithMenuoverFlowOpen() {
         <div>
         <img className="consumer" src={ consumer } alt="text" />
         <MenuOverflowOpen></MenuOverflowOpen>
-        <TreeButton></TreeButton>
-        <MedicalInfoButton></MedicalInfoButton>
         </div>
     );
   }
