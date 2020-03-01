7e0049a4a314889c935285c0d3116597772aa283
RichardPitts1 <Richardepitts@gmail.com>
2020-03-01 13:12:22 -0600

    -

--- src/shared/consumer-dock/consumer-with-dock-opened/consumer-with-dock-opened.js
+++ src/shared/consumer-dock/consumer-with-dock-opened/consumer-with-dock-opened.js
@@ -1,19 +1,7 @@
 import './consumer-with-dock-opened.css';
 import React from 'react'
 import { Link } from "react-router-dom";
-import BalloonWrenchPic from '../../buttons/mac-bar/balloon-wrench-button/balloon-wrench.svg';
-import TwirlPic from '../../buttons/mac-bar/twirl-button/twirl.svg';
-import PreviousTemplatePic from '../../buttons/mac-bar/previous-template-button/previous.svg';
-import NextTemplatePic from '../../buttons/mac-bar/next-template-button/next.svg';
-import NavPic from '../../buttons/mac-bar/nav-button/nav.svg';
-import HistoryPic from '../../buttons/mac-bar/history-button/history.svg';
-import MenuOverflowPic from '../../buttons/mac-bar/menu-overflow/illuminati.svg';
-import BackButtonPic from './images/back.svg';
-import ForwardButtonPic from './images/forward.svg';
-import SnakePic from './images/snake.svg';
-import SearchBar from './images/SearchBar.png';
-import IrisPic from '../../../images/iris.svg'
-import SoundPic from '../../../images/sound.svg'
+import MacBar from '../../mac-bar/mac-bar';
 
 function ConsumerWithDockOpened() {
 
@@ -22,24 +10,7 @@ function ConsumerWithDockOpened() {
         <div class="consumer-dock-panel-parent">
         <Link to="/Consumer">
 
-        <div className="consumer-panel2">
-        <div className="search-bar-parent">
-        <img className="search-bar-png3" src={SearchBar} alt="-" />
-        </div>
-        <img className="back-button-svg" src={ BackButtonPic } alt="some" />
-        <img className="forward-button-svg" src={ ForwardButtonPic } alt="some" />
-            <img className="iris-svg" src={ IrisPic } alt="some" />
-            <img className="balloon-wrench-svg" src={ BalloonWrenchPic } alt="some" />
-            <img className="previous-template-svg" src={ PreviousTemplatePic } alt="some" />
-            <Link to="/BusinessWithDockOpened"><img className="twirl-svg" src={ TwirlPic } alt="some" /></Link>
-            <img className="next-template-svg" src={ NextTemplatePic } alt="some" />
-            <img className="nav-svg" src={ NavPic } alt="some" />
-            <img className="history-svg" src={ HistoryPic } alt="some" />
-            <Link to="/MenuOverflowOpen"><img className="menu-overflow-svg" src={ MenuOverflowPic } alt="some" /></Link>
-            <img className="snake-svg" src={ SnakePic } alt="some" />
-            <img className="sound-svg" src={ SoundPic } alt="some" />
-     
-        </div>
+        <MacBar></MacBar>
         </Link>
         </div>
     );
--- src/shared/mac-bar/mac-bar.js
+++ src/shared/mac-bar/mac-bar.js
@@ -1,39 +1,49 @@
-// import React from 'react';
-// import './mac-bar.css';
-// import { Link } from "react-router-dom";
-// import BalloonWrenchButton from '../buttons/mac-bar/balloon-wrench-button/balloon-wrench-button';
-// import TwirlButton from '../buttons/mac-bar/twirl-button/twirl-button';
-// import NavButton from '../buttons/mac-bar/nav-button/nav-button';
-// import NextTemplateButton from '../buttons/mac-bar/next-template-button/next-template-button';
-// import PreviousTemplateButton from '../buttons/mac-bar/previous-template-button/previous-template-button';
-// import HistoryButton from '../buttons/mac-bar/history-button/history-button';
-// import MenuOverflowButton from '../buttons/mac-bar/menu-overflow/menu-overflow';
+import React from 'react';
+import './mac-bar.css';
+import { Link } from "react-router-dom";
+import BalloonWrenchPic from '../buttons/mac-bar/balloon-wrench-button/balloon-wrench.svg';
+import TwirlPic from '../buttons/mac-bar/twirl-button/twirl.svg';
+import PreviousTemplatePic from '../buttons/mac-bar/previous-template-button/previous.svg';
+import NextTemplatePic from '../buttons/mac-bar/next-template-button/next.svg';
+import NavPic from '../buttons/mac-bar/nav-button/nav.svg';
+import HistoryPic from '../buttons/mac-bar/history-button/history.svg';
+import MenuOverflowPic from '../buttons/mac-bar/menu-overflow/illuminati.svg';
+// import BackButtonPic from '../images/back.svg';
+import ForwardButtonPic from '../../tree/images/forward.svg';
+import SnakePic from '../../tree/images/snake.svg';
+import SearchBar from '../../images/SearchBar.png';
+import IrisPic from '../../images/iris.svg'
+import SoundPic from '../../images/sound.svg'
+
+//        <img className="back-button-svg" src={ BackButtonPic } alt="some" />
+
+export default function MacBar() {
+
+    return (
 
 
-// function MacBar() {
-//     function handleClick() {
-//         console.log("hey");
-//     }
+        <div className="consumer-panel2">
+        <div className="search-bar-parent">
+        <img className="search-bar-png3" src={SearchBar} alt="-" />
+        </div>
 
-//     return (
-
-
-//         <div className="mac-bar-container">
-//         <Link to="/Consumer">
-//         <BalloonWrenchButton></BalloonWrenchButton>
-//         <TwirlButton></TwirlButton>
-//         <NavButton></NavButton>
-//         <PreviousTemplateButton></PreviousTemplateButton>
-//         <NextTemplateButton></NextTemplateButton>
-//         <HistoryButton></HistoryButton>
-//         <MenuOverflowButton></MenuOverflowButton>
-//         <button onClick={handleClick} className="button-child">Refresh</button>
-//         </Link>
-//         </div>
+        <img className="forward-button-svg" src={ ForwardButtonPic } alt="some" />
+            <img className="iris-svg" src={ IrisPic } alt="some" />
+            <img className="balloon-wrench-svg" src={ BalloonWrenchPic } alt="some" />
+            <img className="previous-template-svg" src={ PreviousTemplatePic } alt="some" />
+            <Link to="/BusinessWithDockOpened"><img className="twirl-svg" src={ TwirlPic } alt="some" /></Link>
+            <img className="next-template-svg" src={ NextTemplatePic } alt="some" />
+            <img className="nav-svg" src={ NavPic } alt="some" />
+            <img className="history-svg" src={ HistoryPic } alt="some" />
+            <Link to="/MenuOverflowOpen"><img className="menu-overflow-svg" src={ MenuOverflowPic } alt="some" /></Link>
+            <img className="snake-svg" src={ SnakePic } alt="some" />
+            <img className="sound-svg" src={ SoundPic } alt="some" />
+     
+        </div>
+    
      
 
 
-//     );
-//   }
+    );
+  }
   
-//   export default MacBar;
