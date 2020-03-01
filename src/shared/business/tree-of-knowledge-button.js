36ae3ccd8d386cf2256bb14b8f7f688c4eec3c9e
RichardPitts1 <Richardepitts@gmail.com>
2020-02-29 21:35:14 -0600

    -

--- src/shared/business/tree-of-knowledge-button.js
+++ src/shared/business/tree-of-knowledge-button.js
@@ -6,7 +6,7 @@ export default function BusinessTreeButton() {
 
     return (
         <div class="business-tree-button-parent">
-        <Link to="/BusinessTree">
+        <Link to="/Tree">
         <button className="business-tree-button">Tree of Knowledge</button>
         </Link>
         </div>
--- src/tree/tree-view.js
+++ src/tree/tree-view.js
@@ -2,22 +2,31 @@ import React from 'react'
 import TreePic from '../images/Tree.png';
 import BackButton from '../shared/buttons/back-button';
 import './tree-view.css';
-import TreeWithDockOpened from './tree-with-dock-opened';
+// import TreeWithDockOpened from './tree-with-dock-opened';
 import TreeButtons from './tree-buttons/tree-buttons';
+import ConsumerDock from '../shared/consumer-dock/consumer-dock';
+// import Video from 'react-html5video';
+import TreeVid from '../videos/Tree.mp4';
 
 function TreeView() {
     return (
         <div className="tree-parent">
         <div className="tree-child">
-        <img className="tree-image" src={ TreePic } alt="something"/>
+        <video preload="auto" autoPlay loop className="fullscreen-video" width="1536" height="2048">
+            <source src={TreeVid} 
+                type="video/mp4"></source>
+        </video>
         <div className="tree-dock">
         <BackButton></BackButton>
-        <TreeWithDockOpened></TreeWithDockOpened>
+        <ConsumerDock></ConsumerDock>
         <TreeButtons></TreeButtons>
         </div>
         </div>
         </div>
     );
   }
+
+  
   
   export default TreeView;
+
